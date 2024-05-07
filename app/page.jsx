30ad54cb74c57490ed1aa4dde106ce8105a89dc1
Conor Casey <conorcasey258@gmail.com>
2024-05-07 12:14:26 +0100

    Added component files and home page text

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
-
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      welcome to
+      <br className='max-md:hidden'/>
+      <span className="orange_gradient text-center">ChatGPT Prompt Generaator</span>
+    
+    </h1>
+    <p>
+       This is a tool for creating and sharing prompts for AI platforms such as ChatGPT
+    </p>
+
+     <Feed/>
+    </section>
   )
 }
 
