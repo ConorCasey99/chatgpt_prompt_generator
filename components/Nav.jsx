290cf0f3d4fedaa6008ff9e399a64b97963355e3
Conor Casey <conorcasey258@gmail.com>
2024-05-21 20:40:14 +0100

    Nav Bar Improvements

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,43 +10,71 @@ const Nav = () => {
 
   const [providers, setProviders ] = useState(null);
 
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response)
+    } 
+
+    setProviders();
+  }, [])
+
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className='flex gap-2 flex-center'>
-        <Image 
-          src='/assets/images/logo.svg' alt="logo" width={30} height={30} className='object-contain'
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="images\logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
         />
-        <p className='logo_text'>ChatGPT Prompts</p>
+        <p className="logo_text">ChatGPT Prompts</p>
       </Link>
 
-      <div className='sm:flex hidden'>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
         {isUserLoggedIn ? (
-          <div className='flex gap-3 md:gap-5'>
-            <Link href="/create-prompt" className='black_btn'>
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
-            <button type="button" onClick={signOut} className='outline_btn'>
+
+            <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
-            
+
             <Link href="/profile">
-              <Image 
-                src="/assets/images/logo.svg"
+              <Image
+                src={"images\logo.svg"}
                 width={37}
                 height={37}
-                className='rounded-full'
+                className="rounded-full"
                 alt="profile"
-              />  
+              />
             </Link>
           </div>
-        ): (
+        ) : (
           <>
-            
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
           </>
         )}
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav
